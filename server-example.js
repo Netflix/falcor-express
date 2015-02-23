b75618c0f86b1b827ee6f5fbc9fed445ae6d0970
Michael Paulson <mpaulson@netflix.com>
2015-02-23 14:30:12 -0800

    Removed old code and stripped this project down to the bare bones.

--- server-example.js
+++ server-example.js
@@ -1,27 +1,16 @@
 var http = require('http');
-var falkor = require('./lib/Falkor');
+var falkor = require('falcor');
 var Rx = require('rx');
 var url = require('url');
 var model = new falkor.JSONGModel(null, 0, 0, require('./Cache')());
-var FalkorEndpoint = require('./');
+var FalkorEndpoint = require('./../index');
 var falkorEndpoint = new FalkorEndpoint(model);
 
 http.
     createServer(function(req, res) {
         var urlValue = url.parse(req.url);
-        var context = urlValue.query.
-            split('&').
-            reduce(function(acc, q) {
-                var queryParam = q.split('=');
-                acc[queryParam[0]] = decodeURIComponent(queryParam[1]);
-                if (queryParam[0] === 'path') {
-                    // optional, Falkor endpoint will take care of that.
-                    acc[queryParam[0]] = JSON.parse(acc[queryParam[0]]);
-                }
-                return acc;
-            }, {});
         if (urlValue.path.indexOf('/falkor') === 0) {
-            falkorEndpoint.execute(context, function(err, jsonString) {
+            falkorEndpoint.fromHttpRequest(req, function(err, jsonString) {
                 if (err) {
                     res.writeHead(500);
                     res.write(err);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+var Rx = require('rx');
 var url = require('url');
 
 /**
@@ -43,6 +44,25 @@ FalkorEndpoint.prototype = {
             }, function(err) {
                 callback(err);
             });
+    },
+    
+    fromHttpRequest: function(req, cb) {
+        var urlValue = url.parse(req.url);
+        var context = {};
+        if (urlValue.query) {
+            context = urlValue.query.
+                split('&').
+                reduce(function(acc, q) {
+                    var queryParam = q.split('=');
+                    acc[queryParam[0]] = decodeURIComponent(queryParam[1]);
+                    if (queryParam[0] === 'path') {
+                        // optional, Falkor endpoint will take care of that.
+                        acc[queryParam[0]] = JSON.parse(acc[queryParam[0]]);
+                    }
+                    return acc;
+                }, {});
+        }
+        this.execute(context, cb);
     }
 };
 
