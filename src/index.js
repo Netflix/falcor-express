f69ae8d1f2d80dbc6a3ce57c44b969857900f862
Michael Paulson <mpaulson@netflix.com>
2015-05-14 16:12:57 -0700

    made the express middleware use a function to return the dataSource.

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@
 var requestToContext = require("./requestToContext");
 var FalcorEndpoint = module.exports = {};
 
-FalcorEndpoint.expressMiddleware = function(dataSource) {
-    return function(req, reply) {
+FalcorEndpoint.expressMiddleware = function(getDataSource) {
+    return function(req, res) {
+        var dataSource = getDataSource(req, res);
         var context = requestToContext(req);
         // probably this should be sanity check function?
         if (Object.keys(context).length === 0) {
-            return reply.status(500).send("Request not supported");
+            return res.status(500).send("Request not supported");
         }
         if (typeof context.method === "undefined" || context.method.length === 0) {
-            return reply.status(500).send("No query method provided");
+            return res.status(500).send("No query method provided");
         }
         if (typeof dataSource[context.method] === "undefined") {
-            return reply.status(500).send("Data source does not implement the requested method");
+            return res.status(500).send("Data source does not implement the requested method");
         }
         dataSource[context.method]([].concat(context.path)).subscribe(function(jsong) {
-            reply.status(200).send(JSON.stringify(jsong));
+            res.status(200).send(JSON.stringify(jsong));
         }, function(err) {
-            reply.status(500).send(err);
+            res.status(500).send(err);
         });
     };
 };
